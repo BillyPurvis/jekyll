df71388b5fd9f2c4ace9a365e2b7140a01a475b5
Billy Purvis <Billy Purvis>
2015-12-25 17:29:52 +0000

    new file paths

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,7 @@ gulp.task('watch', function () {
     log('Watch files for changes');
     gulp.watch('./_sass/*.scss', ['sass', 'jekyll']);
     gulp.watch('./js/*.js', ['compressJs']);
-    gulp.watch(['index.html', '_includes/*.html', '_layouts/*.html', '_posts/*.html', '*/*.html', '*/*.md'], ['jekyll']);
+    gulp.watch(['**/*.html', '*/*.md'], ['jekyll']);
 });
 
 // Run sequentually. 
